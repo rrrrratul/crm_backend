25887de0c60dda0847e20ce8b73759e8a2948cb3
rrrrratul <rrrrratul@gmail.com>
2023-08-14 13:48:42 +0530

    Deploying the BE

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    DB_NAME: "crm_db",
-    DB_URL:  process.env.MONGODB_URI || "mongodb://localhost/crm_db"
-}
+  DB_NAME: "crm_db",
+  DB_URL:
+    process.env.MONGODB_URI ||
+    `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD}@cluster0.7svvvwe.mongodb.net/crm_db`,
+};
